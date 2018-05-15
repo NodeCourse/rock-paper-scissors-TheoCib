955e267d407f4ed3f21a557ecf838e5c199e9950
TheoCib <theophile.cibert@ynov.com>
2018-05-15 09:48:14 +0200

    Fini !

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
+const randomItem =require('random-item')
 const app = express();
 const choices = require('./skilss.json');
 app.set('view engine', 'pug');
 
 app.use(express.static("public"));
 
+function getWinner(userChoice, computerChoice) {
+    if(userChoice.winOver.includes(computerChoice.id)){
+        return userChoice;
+    }
+
+    if(computerChoice.winOver.includes(userChoice.id)){
+        return computerChoice;
+    }
+
+    return null;
+}
+
 app.get('/',(request, response) => {
     response.render("home", {choices: choices});
 });
 
 
-app.get('/result',(request, response) => {
-    response.render("result");
+app.get('/play/:choice',(request, response) => {
+    console.log(request.params.choice);
+    const userChoice = choices.find((choice) => {
+        return choice.id === request.params.choice;
+    });
+    const computerChoice = randomItem(choices);
+    const winner = getWinner(userChoice, computerChoice);
+
+    let winnerName;
+    if(winner===userChoice){
+        winnerName = 'Computer';
+    }
+    else if(winner===computerChoice){
+        winnerName = 'User';
+    }
+
+    response.render("play", { userChoice, computerChoice, winner , winnerName});
 });
 
 
